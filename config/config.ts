ec053b96232c19fb5f81882821c106385b7205a2
10331 <shichengk19@gmail.com>
2024-11-06 17:14:22 +0800

    2024-11-06

---
+++ config/config.ts
@@ -0,0 +1,30 @@
+// import { Sequelize } from "sequelize"
+
+// export class database {
+//     sequelize = new Sequelize('ERP', 'ERP_USER', 'ShiCheng1999$', {
+//         host: '127.0.0.1',
+//         dialect: 'mssql',
+//         dialectOptions: {
+//             options: {
+//                 encrypt: false,
+//                 trustServerCertificate: false
+//             }
+//         }
+//     });
+// }
+
+import { Dialect } from "sequelize";
+
+export const development = {
+    username: "ERP_USER",
+    password: "ShiCheng1999$",
+    database: "ERP",
+    host: "localhost",
+    dialect: "mssql" as Dialect,
+    migrationStoragePath: "src/migrations",
+    seederStoragePath: "src/seeders",
+    ssl: {
+        rejectUnauthorized: false // 忽略 SSL 驗證
+    }
+};
+
---
+++ src/models/config.ts
@@ -0,0 +1,23 @@
+// import { DataTypes } from 'sequelize';
+// import { database } from '../config/database'
+
+// var connect = new database
+// var User = connect.sequelize.define('User', {
+//     id: {
+//         type: DataTypes.INTEGER,
+//         primaryKey: true,
+//         autoIncrement: true
+//     },
+//     account: {
+//         type: DataTypes.STRING
+//     },
+//     password: {
+//         type: DataTypes.STRING
+//     }
+// }, {
+//     tableName: 'User', // 指定現有資料表名
+//     freezeTableName: true, // 不要讓 Sequelize 自動改變表名
+//     timestamps: false // 如果沒有 `createdAt` 和 `updatedAt` 欄位，關閉時間戳
+// });
+
+// export default User
